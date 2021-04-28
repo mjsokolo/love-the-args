44637084cded1e24263abe27360777529d57a629
Michael <msokolovsky@wpi.edu>
2021-04-28 04:07:09 -0400

    more changes

--- src/Chunk2.js
+++ src/Chunk2.js
@@ -3,12 +3,58 @@ import './Chunk.css';
 import TextareaAutosize from 'react-autosize-textarea';
 // import Note from './Note';
 
+const ArgParts = {
+  names: {
+    id1: 'statement',
+    id2: 'kushya',
+    id3: 'teirutz',
+    id4: 'sheelah',
+    id5: 'tshuvah',
+    id6: 'reayah',
+    id7: 'siyua',
+    id8: 'narrative',
+    id9: 'maskanah',
+  },
+  borders: {
+    id1: 'black',
+    id2: 'darkcyan',
+    id3: 'cyan',
+    id4: 'darkgreen',
+    id5: 'olive',
+    id6: 'blue',
+    id7: 'grey',
+    id8: 'pink',
+    id9: 'red',
+  },
+};
+
+const RenderArgPartButtons = (argparts, handleArgPart) => {
+  const buttons = Object.keys(argparts.names).map((id) => (
+    <div>
+      <button
+        type="button"
+        onClick={() => handleArgPart(id)}
+        style={{
+          color: argparts.borders[id],
+        }}
+      >
+        {argparts.names[id]}
+      </button>
+      {/* <br></br> */}
+    </div>
+  ));
+  return buttons;
+};
+
 const RenderChunks = (state, handleTextChange) => {
   const chunks = state.order.map((id) => (
     <TextareaAutosize
       id={id}
       class="chunk"
-      style={{ marginRight: state.indents[id] * 10 }}
+      style={{
+        marginRight: state.indents[id] * 10,
+        borderColor: state.borders[id],
+      }}
       onSelect={() => {
         handleTextChange(id);
       }}
@@ -33,6 +79,7 @@ class Chunk2 extends React.Component {
       order: ['id1'],
       indents: { id1: 0 },
       txts: { id1: '' },
+      borders: { id1: 'black' },
     };
     this.totalChunks = 1;
     this.handleTextChange = this.handleTextChange.bind(this);
@@ -40,6 +87,7 @@ class Chunk2 extends React.Component {
     this.handleMergeUp = this.handleMergeUp.bind(this);
     this.handleTabOut = this.handleTabOut.bind(this);
     this.handleTabIn = this.handleTabIn.bind(this);
+    this.handleArgPart = this.handleArgPart.bind(this);
   }
 
   handleTextChange(id) {
@@ -114,25 +162,42 @@ class Chunk2 extends React.Component {
     });
   }
 
+  handleArgPart(arg) {
+    console.log('test');
+    const id = this.state.activeId;
+    const border = ArgParts.borders[arg];
+    console.log(arg, id, border);
+    this.setState({
+      borders: { ...this.state.borders, [id]: border },
+    });
+  }
+
   render() {
     return (
-      <div>
-        <button type="button" onClick={this.handleSplit}>
-          split
-        </button>
-        <button type="button" onClick={this.handleMergeUp}>
-          merge up
-        </button>
-        <button type="button" onClick={this.handleTabOut}>
-          &lt;&lt;&lt;
-        </button>
-        <button type="button" onClick={this.handleTabIn}>
-          &gt;&gt;&gt;
-        </button>
-        {RenderChunks(this.state, this.handleTextChange)}
-        {this.state.caret}
+      <div class="grid-container">
+        <div class="item1">
+          {RenderArgPartButtons(ArgParts, this.handleArgPart)}
+        </div>
+        <div class="item2">
+          <button type="button" onClick={this.handleSplit}>
+            split
+          </button>
+          <button type="button" onClick={this.handleMergeUp}>
+            merge up
+          </button>
+          <button type="button" onClick={this.handleTabOut}>
+            &lt;&lt;&lt;
+          </button>
+          <button type="button" onClick={this.handleTabIn}>
+            &gt;&gt;&gt;
+          </button>
+        </div>
+        <div class="item3">
+          {RenderChunks(this.state, this.handleTextChange)}
+        </div>
+        {/* {this.state.caret}
         {this.state.activeId}
-        {this.state.order}
+        {this.state.order} */}
       </div>
     );
   }
