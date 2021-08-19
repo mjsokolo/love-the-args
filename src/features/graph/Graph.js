77d3774d2e7c4bf30a741962a4589c32f007cf16
Michael <msokolovsky@wpi.edu>
2021-08-19 15:36:03 -0400

    minor changes to node and text heights

--- src/features/graph/Graph.js
+++ src/features/graph/Graph.js
@@ -3,7 +3,7 @@ import './css/Graph.css';
 import Nodes from './Nodes';
 import SequentialPath from './SequentialPath';
 import GraphContextMenu from './GraphContextMenu';
-import ConnectionContextMenu from './ConnectionContextMenu'
+import ConnectionContextMenu from './ConnectionContextMenu';
 import ConnectionListener from './ConnectionListener';
 import NodeConnections from './Connections';
 
