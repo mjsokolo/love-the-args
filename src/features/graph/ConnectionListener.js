bae1d08b5df7e130721010ec54b7b88dfa50129b
Michael <msokolovsky@wpi.edu>
2021-08-23 03:05:12 -0400

    [Bugfix] more robust connection placement

--- src/features/graph/ConnectionListener.js
+++ src/features/graph/ConnectionListener.js
@@ -1,36 +1,45 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+function returnNodeParent(target) {
+  // returns closest parent of target if
+  // if parent is class 'node' or 'react-contextmenu-item
+  if (target === document) {
+    return false;
+  }
+  if (target.classList.contains('react-contextmenu-item')) {
+    return target;
+  }
+  if (target.classList.contains('node')) {
+    return target;
+  }
+  return returnNodeParent(target.parentNode);
+}
+
 function onClick(event, graph, dispatch) {
-  switch (event.target.classList[0]) {
-    case 'react-contextmenu-item': {
-      // Entered Connection Mode
-      break;
-    }
-    case 'node': {
-      if (graph.selectedNode !== null) {
-        // console.log('In Connection Mode');
-        if (graph.selectedNode === event.target.id) {
-          // console.log('Same ID Clicked');
-        } else if (
-          graph.connections.includes([graph.selectedNode, event.target.id])
-        ) {
-          // console.log('Connection Already in Present');
-        } else {
-          dispatch({
-            type: 'addConnection',
-            payload: {
-              connection: [graph.selectedNode, event.target.id, graph.mode],
-            },
-          });
-        }
+  const target = returnNodeParent(event.target);
+
+  if (target === false) {
+    dispatch({ type: 'resetMode' });
+  } else if (target.classList[0] === 'react-contextmenu-item') {
+    // Entered Connection Mode
+  } else if (target.classList[0] === 'node') {
+    if (graph.selectedNode !== null) {
+      // console.log('In Connection Mode');
+      if (graph.selectedNode === target.id) {
+        // console.log('Same ID Clicked');
+      } else if (graph.connections.includes([graph.selectedNode, target.id])) {
+        // console.log('Connection Already in Present');
+      } else {
+        dispatch({
+          type: 'addConnection',
+          payload: {
+            connection: [graph.selectedNode, target.id, graph.mode],
+          },
+        });
       }
-      dispatch({ type: 'resetMode' });
-      break;
     }
-    default:
-      dispatch({ type: 'resetMode' });
-      break;
+    dispatch({ type: 'resetMode' });
   }
 }
 
